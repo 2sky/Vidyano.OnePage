77d075ce57dfbe741edf94d04cd03f20ea66628b
David Sleeckx <david@sleeckx.be>
2017-04-02 13:15:40 +0200

    Intercept events when origin is vi-scroller
    Fixed error when navigation is not available
    Updated version

--- WebComponents/OnePageWrapper/one-page-wrapper.ts
+++ WebComponents/OnePageWrapper/one-page-wrapper.ts
@@ -21,10 +21,53 @@ namespace Vidyano.WebComponents {
     }, "vi")
     export class OnePageWrapper extends Vidyano.WebComponents.WebComponent {
         private _activeAnchor: HTMLAnchorElement;
+        private _routesObserver: PolymerDomChangeObserver;
         isLoading: boolean; private _setIsLoading: (isLoading: boolean) => void;
         size: Vidyano.WebComponents.ISize;
         routes: Vidyano.WebComponents.AppRoute[];
         swiper: Swiper;
+
+        attached() {
+            super.attached();
+
+            if (!this._routesObserver)
+                this._routesObserver = Polymer.dom(this.$.routes).observeNodes(this._routesChanged.bind(this));
+        }
+
+        detached() {
+            super.detached();
+
+            Polymer.dom(this.$.routes).unobserveNodes(this._routesObserver);
+            this._routesObserver = null;
+        }
+
+        private _routesChanged(info: PolymerDomChangedInfo) {
+            info.addedNodes.forEach(n => {
+                const intercepts: { touchstart?: EventListenerOrEventListenerObject; mousedown?: EventListenerOrEventListenerObject; mousewheel?: EventListenerOrEventListenerObject; } = {};
+                n.addEventListener("mousewheel", intercepts.mousewheel = this._intercept.bind(this));
+                n.addEventListener("mousedown", intercepts.mousedown = this._intercept.bind(this));
+                n.addEventListener("touchstart", intercepts.touchstart = this._intercept.bind(this));
+                n["__viScrollerIntercepts"] = intercepts;
+            });
+
+            info.removedNodes.forEach(n => {
+                const intercepts: { touchstart?: EventListenerOrEventListenerObject; mousedown?: EventListenerOrEventListenerObject; mousewheel?: EventListenerOrEventListenerObject; } = {};
+                n["__viScrollerIntercepts"] = intercepts;
+
+                if (!intercepts)
+                    return;
+
+                n.removeEventListener("mousewheel", intercepts.mousewheel);
+                n.removeEventListener("mousedown", intercepts.mousedown);
+                n.removeEventListener("touchstart", intercepts.touchstart);
+            });
+        }
+
+        private _intercept(e: Event) {
+            const scrollerOrMe = this.findParent((e: HTMLElement) => e instanceof Vidyano.WebComponents.Scroller || e === this, <HTMLElement>e.target);
+            if (scrollerOrMe instanceof Vidyano.WebComponents.Scroller && scrollerOrMe.innerHeight > scrollerOrMe.outerHeight)
+                e.stopPropagation();
+        }
 
         private _updateSwiper(size: Vidyano.WebComponents.ISize, routes: Vidyano.WebComponents.AppRoute[]) {
             this.$["swiperContainer"].style.width = `${size.width}px`;
@@ -55,23 +98,28 @@ namespace Vidyano.WebComponents {
             if (!mappedPathRoute) // 404
                 return;
 
-            const currentRoute = this.app["routeMap"][Vidyano.WebComponents.App.removeRootPath(mappedPathRoute.path)];
+            setTimeout(() => {
+                if (this.swiper)
+                    return;
 
-            this.swiper = new Swiper(this.$["swiperContainer"], {
-                direction: "vertical",
-                slidesPerView: 1,
-                paginationClickable: true,
-                mousewheelControl: true,
-                setWrapperSize: true,
-                roundLengths: true,
-                initialSlide: this.routes.indexOf(currentRoute),
-                onSlideChangeStart: this._slideChanged.bind(this),
-                slideClass: "swiper-slide",
-                slideActiveClass: "swiper-active"
-            });
+                const currentRoute = this.app["routeMap"][Vidyano.WebComponents.App.removeRootPath(mappedPathRoute.path)];
 
-            this._setIsLoading(false);
-            this._updateActiveAnchor(this.swiper);
+                this.swiper = new Swiper(this.$["swiperContainer"], {
+                    direction: "vertical",
+                    slidesPerView: 1,
+                    paginationClickable: true,
+                    mousewheelControl: true,
+                    setWrapperSize: true,
+                    roundLengths: true,
+                    initialSlide: this.routes.indexOf(currentRoute),
+                    onSlideChangeStart: this._slideChanged.bind(this),
+                    slideClass: "swiper-slide",
+                    slideActiveClass: "swiper-active"
+                });
+
+                this._setIsLoading(false);
+                this._updateActiveAnchor(this.swiper);
+            }, 250);
         }
 
         private _slideChanged(swiper: Swiper) {
@@ -79,12 +127,16 @@ namespace Vidyano.WebComponents {
             this._updateActiveAnchor(swiper);
         }
 
-        private _updateActiveAnchor(swiper: Swiper) {
+        private async _updateActiveAnchor(swiper: Swiper) {
+            await this.app.initialize;
+            Polymer.dom(this).flush();
+
             if (this._activeAnchor)
                 this._activeAnchor.classList.remove("active");
 
             this._activeAnchor = <HTMLAnchorElement>this.$["nav"].querySelector(`a:nth-of-type(${swiper.activeIndex + 1})`);
-            this._activeAnchor.classList.add("active");
+            if (this._activeAnchor)
+                this._activeAnchor.classList.add("active");
         }
 
         private _changeSlide(e: TapEvent) {
